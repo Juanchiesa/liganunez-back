1d8b87009f9b71d2bee2fff6ad2e85ce6703e476
TamboriniAgustin <tamboriniagustin00@gmail.com>
2023-07-25 17:00:50 -0300

    Fix scheduled task

--- src/main/java/com/venedicto/liganunez/schedule/ScheduledTasks.java
+++ src/main/java/com/venedicto/liganunez/schedule/ScheduledTasks.java
@@ -4,7 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
 import com.venedicto.liganunez.service.PictureService;
@@ -12,10 +13,23 @@ import com.venedicto.liganunez.service.PictureService;
 @Component
 public class ScheduledTasks {
 	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+	private final ThreadPoolTaskScheduler taskScheduler;
 	@Autowired
 	private PictureService pictureService;
 	
-	@Scheduled(cron = "0 0 0 1 1/3 ?")
+	public ScheduledTasks() {
+        taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.initialize();
+    }
+
+    public void startScheduledTask() {
+        taskScheduler.schedule(this::deletePictures, new CronTrigger("0 0 0 1 1/3 ?")); //Expresión cron para ejecutar cada 3 meses
+    }
+
+    public void stopScheduledTask() {
+        taskScheduler.shutdown();
+    }
+    
 	@Profile("production")
     public void deletePictures() {
 		log.info("[Proceso trimestral] Iniciando limpieza de imagenes...");
