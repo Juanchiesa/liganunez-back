a71f6e6a087feedb27433772179f89037062c91e
TamboriniAgustin <tamboriniagustin00@gmail.com>
2023-08-29 17:52:57 -0300

    Nueva config de CORS

--- src/main/java/com/venedicto/liganunez/configuration/security/CorsConfiguration.java
+++ src/main/java/com/venedicto/liganunez/configuration/security/CorsConfiguration.java
@@ -9,9 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("POST", "GET", "PATCH", "DELETE", "PUT");
-    } 
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins("https://stg.liganunez.com.ar")
+	            .allowedMethods("POST", "GET", "PATCH", "DELETE", "PUT");
+	}
 }
